90b8a1a53a01a9f914e5874c96f644672291e085
danielarnost <danielarnost@utexaas.edu>
2016-11-17 11:49:27 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,51 +1,11 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'Kyanite@55',
-	database: 'burgers_db'
-});
-
-connection.connect(function (err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
-});
-
-module.exports = connection;
-
-
-
-
-
-
-
-
-
-
-//=========================================================================
 // var mysql = require('mysql');
-// var source = {
-	
-// localhost:{
+// var connection = mysql.createConnection({
 // 	port: 3306,
 // 	host: 'localhost',
-// 	user: '',
+// 	user: 'root',
 // 	password: 'Kyanite@55',
 // 	database: 'burgers_db'
-// 			},
-//  jawsDB: {
-//         port: 3306,
-//         host: 'enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         user: 'fctrltohm7axdmwp',
-//         password: "e4gi3ibn2ih9izoj",
-//         database: "k2uvg0tggsovzoex"
-// 		}
-// }
-// var connection = mysql.createConnection(source.jawsDB);
+// });
 
 // connection.connect(function (err) {
 // 	if (err) {
@@ -58,5 +18,67 @@ module.exports = connection;
 // module.exports = connection;
 
 
+
+
+
+
+
+
+
+
+//=========================================================================
+var mysql = require('mysql');
+var source = {
+	
+localhost:{
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: 'Kyanite@55',
+	database: 'burgers_db'
+			},
+ jawsDB: {
+        port: 3306,
+        host: 'enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'fctrltohm7axdmwp',
+        password: "e4gi3ibn2ih9izoj",
+        database: "k2uvg0tggsovzoex"
+		}
+}
+
+var connection = mysql.createConnection(source.jawsDB);
+
+
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
+
+
 //mysql://OLDUSER:OLDPASS@OLDHOST/OLDDATABASE?…
 //mysql://fctrltohm7axdmwp:e4gi3ibn2ih9izoj@enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k2uvg0tggsovzoex
+
+//====================================
+// var mysql = require('mysql');
+// var connection;
+
+// if (process.env.JAWSDB_URL) {
+// connection = mysql.createConnection(process.env.jawsDB);
+// }else{
+// mysql.createConnection({
+// 	port: 3306,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'Kyanite@55',
+// 	database: 'burgers_db'
+// });
+// };
+
+// connection.connect();
+// module.exports = connection
