02a95277b63d8c5cf322df2ba8a27e2c9a94213e
danielarnost <danielarnost@utexaas.edu>
2016-12-05 12:42:02 -0600

    fixed, no jaws

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-// 	port: 3306,
-// 	host: 'localhost',
-// 	user: 'root',
-// 	password: 'Kyanite@55',
-// 	database: 'burgers_db'
-// });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: 'Kyanite@55',
+	database: 'burgers_db'
+});
 
-// connection.connect(function (err) {
-// 	if (err) {
-// 		console.error('error connecting: ' + err.stack);
-// 		return;
-// 	}
-// 	console.log('connected as id ' + connection.threadId);
-// });
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
 
-// module.exports = connection;
+module.exports = connection;
 
 
 
@@ -36,17 +36,17 @@ localhost:{
 	user: 'root',
 	password: 'Kyanite@55',
 	database: 'burgers_db'
-			},
- jawsDB: {
-        port: 3306,
-        host: 'enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'fctrltohm7axdmwp',
-        password: "e4gi3ibn2ih9izoj",
-        database: "k2uvg0tggsovzoex"
-		}
+			}
+//  jawsDB: {
+//         port: 3306,
+//         host: 'enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         user: 'fctrltohm7axdmwp',
+//         password: "e4gi3ibn2ih9izoj",
+//         database: "k2uvg0tggsovzoex"
+// 		}
 }
 
-var connection = mysql.createConnection(source.jawsDB);
+// var connection = mysql.createConnection(source.jawsDB);
 
 
 
